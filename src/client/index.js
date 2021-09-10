85726ba7066bf5f8257724f70ea2cae1b393eed2
ilexwg <ilexwg@gmail.com>
2021-09-10 12:22:06 +0800

    add plugin

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,4 @@
 import { handleSubmit } from './js/formHandler';
 import { checkForName } from './js/nameChecker';
 
-console.log(checkForName, '👀');
+console.log('Hello webpack!', '👀');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+
   module: {
     rules: [
       {
@@ -12,4 +14,11 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
