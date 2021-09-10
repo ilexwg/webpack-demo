da85d75c7aa90f5cd805161a7fd7a9fa3b72f07d
ilexwg <ilexwg@gmail.com>
2021-09-10 13:14:25 +0800

    add mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: './src/client/index.js',
 
   module: {
@@ -18,7 +20,7 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/client/views/index.html',
-      filename: './index.html',
+      filename: './index2.html',
     }),
   ],
 };
